6adfc77d95a72fd4f0e861a0b810c2fc63ef7ba2
Sarthak Handa <sarthakh23@gmail.com>
2026-04-21 17:18:38 +0530

    ijij

--- assets/js/config.js
+++
@@ -1,3 +0,0 @@
-const API_URL = "/api";
-// const API_URL = "http://localhost:5000"; //FOR TESTING ON LOCAL SERVER
-// const API_URL = "https://s9zg3g8p-5000.inc1.devtunnels.ms"; //FOR TESTING ON LOCAL SERVER
