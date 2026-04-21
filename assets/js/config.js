63dc2f89c7a824c1275b06b3779d7a3568fa6aef
Sarthak Handa <sarthakh23@gmail.com>
2026-04-21 11:07:31 +0530

    real

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,3 +1,3 @@
-const API_URL = "https://ydprojects.ydagchmi.com/api";
+const API_URL = "/api";
 // const API_URL = "http://localhost:5000"; //FOR TESTING ON LOCAL SERVER
 // const API_URL = "https://s9zg3g8p-5000.inc1.devtunnels.ms"; //FOR TESTING ON LOCAL SERVER
